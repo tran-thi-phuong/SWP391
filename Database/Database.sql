3544dd9b844eca37e5dbea5dbd6b330fbe8258a4
tuando100704 <146158533+tuando100704@users.noreply.github.com>
2024-09-16 23:16:26 +0700

    Update Database.sql

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,4 +1,4 @@
-﻿-- Tạo cơ sở dữ liệu OnlineLearning
+-- Tạo cơ sở dữ liệu OnlineLearning
 CREATE DATABASE OnlineLearning;
 GO
 
@@ -6,214 +6,214 @@ GO
 USE OnlineLearning;
 GO
 
--- Tạo bảng User
-CREATE TABLE Users (
-    UserId INT PRIMARY KEY IDENTITY(1,1),
-    username NVARCHAR(50) NOT NULL UNIQUE,
-    password NVARCHAR(255) NOT NULL,
-    name NVARCHAR(100) NOT NULL,
-	gender NVARCHAR(10) NOT NULL,
-    phone NVARCHAR(15),
-    email NVARCHAR(100) NOT NULL,
-	address NVARCHAR(255) NOT NULL,
-	avatar NVARCHAR(255) NOT NULL,
-    role NVARCHAR(20) Not Null, 
-	status NVARCHAR(10) NOT NULL,
+-- Tạo bảng Users
+CREATE TABLE Users(
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(255) NOT NULL,
+    Name NVARCHAR(100) NOT NULL,
+	Gender NVARCHAR(10),
+    Phone NVARCHAR(15),
+    Email NVARCHAR(100) NOT NULL,
+	Address NVARCHAR(255),
+	Avatar NVARCHAR(255),
+    Role NVARCHAR(20) Not Null, 
+	Status NVARCHAR(10) NOT NULL
 );
 GO
 
 -- Tạo bảng Blog_Category
 CREATE TABLE Blog_Category (
-    Blog_CategoryId INT PRIMARY KEY IDENTITY(1,1),
-    title NVARCHAR(100) NOT NULL
+    Blog_CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(100) NOT NULL
 );
 GO
 
 -- Tạo bảng Subject_Category
 CREATE TABLE Subject_Category (
-    Subject_CategoryId INT PRIMARY KEY IDENTITY(1,1),
-    title NVARCHAR(100) NOT NULL
+    Subject_CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(100) NOT NULL
 );
 GO
 
--- Tạo bảng Blog
-CREATE TABLE Blog (
+-- Tạo bảng Blogs
+CREATE TABLE Blogs (
     BlogId INT PRIMARY KEY IDENTITY(1,1),
-    UserId INT,
-    title NVARCHAR(255) NOT NULL,
-    content NVARCHAR(MAX) NOT NULL,
-    create_at DATETIME DEFAULT GETDATE(),
-    Blog_CategoryId INT,
-    FOREIGN KEY (UserId) REFERENCES Users(UserId),
-    FOREIGN KEY (Blog_CategoryId) REFERENCES Blog_Category(Blog_CategoryId)
+    UserID INT,
+    Title NVARCHAR(255) NOT NULL,
+    Content NVARCHAR(MAX) NOT NULL,
+    Create_At DATETIME DEFAULT GETDATE(),
+    Blog_CategoryID INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (Blog_CategoryID) REFERENCES Blog_Category(Blog_CategoryID)
 );
 GO
 
--- Tạo bảng Subject
+-- Tạo bảng Subjects
 CREATE TABLE Subjects (
-    SubjectId INT PRIMARY KEY IDENTITY(1,1),
-    title NVARCHAR(255) NOT NULL,
-    description NVARCHAR(MAX),
-    Subject_CategoryId INT,
-    status NVARCHAR(50),
-    thumbnail NVARCHAR(MAX),
-    update_date Datetime,
-    FOREIGN KEY (Subject_CategoryId) REFERENCES Subject_Category(Subject_CategoryId)
+    SubjectID INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(255) NOT NULL,
+    Description NVARCHAR(MAX),
+    Subject_CategoryID INT,
+    Status NVARCHAR(50),
+    Thumbnail NVARCHAR(MAX),
+    Update_Date Datetime,
+    FOREIGN KEY (Subject_CategoryID) REFERENCES Subject_Category(Subject_CategoryID)
 );
 GO
 
--- Tạo bảng Test
-CREATE TABLE Test (
-    TestId INT PRIMARY KEY IDENTITY(1,1),
-    SubjectId INT,
-    title NVARCHAR(255) NOT NULL,
-    description NVARCHAR(MAX),
-    type NVARCHAR(50),
-    duration INT,
-    pass_rate DECIMAL(5, 2),
-    level NVARCHAR(50),
-    quantity INT,
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId)
+-- Tạo bảng Tests
+CREATE TABLE Tests (
+    TestID INT PRIMARY KEY IDENTITY(1,1),
+    SubjectID INT,
+    Title NVARCHAR(255) NOT NULL,
+    Description NVARCHAR(MAX),
+    Type NVARCHAR(50),
+    Duration INT,
+    Pass_Condition DECIMAL(5, 2),
+    Level NVARCHAR(50),
+    Quantity INT,
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID)
 );
 GO
 
--- Tạo bảng Lesson
-CREATE TABLE Lesson (
-    LessonId INT PRIMARY KEY IDENTITY(1,1),
-    SubjectId INT,
-    title NVARCHAR(255) NOT NULL,
-    type NVARCHAR(50),
-    content NVARCHAR(MAX),
-    video_link NVARCHAR(255),
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId)
+-- Tạo bảng Lessons
+CREATE TABLE Lessons (
+    LessonID INT PRIMARY KEY IDENTITY(1,1),
+    SubjectID INT,
+    Title NVARCHAR(255) NOT NULL,
+    Type NVARCHAR(50),
+    Content NVARCHAR(MAX),
+    Video_Link NVARCHAR(255),
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID)
 );
 GO
 
 -- Tạo bảng Package_Price
 CREATE TABLE Package_Price (
-    PackageId INT PRIMARY KEY IDENTITY(1,1),
-    SubjectId INT,
-    name NVARCHAR(255) NOT NULL,
-    duration_time INT,
-    sale_price DECIMAL(10, 2),
-    price DECIMAL(10, 2),
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId)
+    PackageID INT PRIMARY KEY IDENTITY(1,1),
+    SubjectID INT,
+    Name NVARCHAR(255) NOT NULL,
+    Duration_time INT,
+    Sale_Price DECIMAL(10, 2),
+    Price DECIMAL(10, 2),
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID)
 );
 GO
 -- Tạo bảng Registrations
 CREATE TABLE Registrations (
-    RegistrationId INT PRIMARY KEY IDENTITY(1,1),
-    UserId INT,
-    SubjectId INT,
-    PackageId INT,
-    registration_time DATETIME DEFAULT GETDATE(),
-    valid_to DATE,
-    status NVARCHAR(50),
-    FOREIGN KEY (UserId) REFERENCES Users(UserId),
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId),
-	FOREIGN KEY (PackageId) REFERENCES Package_Price(PackageId)
+    RegistrationID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT,
+    SubjectID INT,
+    PackageID INT,
+    Registration_Time DATETIME DEFAULT GETDATE(),
+    Valid_To DATE,
+    Status NVARCHAR(50),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID),
+	FOREIGN KEY (PackageID) REFERENCES Package_Price(PackageID)
 	
 );
 GO
 
 
--- Tạo bảng Slider
-CREATE TABLE Slider (
-    SliderId INT PRIMARY KEY IDENTITY(1,1),
-    BlogId INT,
-    SubjectId INT,
-    title NVARCHAR(255) NOT NULL,
-    image NVARCHAR(255),
-    content NVARCHAR(MAX),
-    FOREIGN KEY (BlogId) REFERENCES Blog(BlogId),
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId)
+-- Tạo bảng Sliders
+CREATE TABLE Sliders (
+    SliderID INT PRIMARY KEY IDENTITY(1,1),
+    BlogID INT,
+    SubjectID INT,
+    Title NVARCHAR(255) NOT NULL,
+    Image NVARCHAR(255),
+    Content NVARCHAR(MAX),
+    FOREIGN KEY (BlogID) REFERENCES Blogs(BlogID),
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID)
 );
 GO
 
--- Tạo bảng Question
-CREATE TABLE Question (
-    QuestionId INT PRIMARY KEY IDENTITY(1,1),
-    SubjectId INT,
-    LessonId INT,
-    content NVARCHAR(MAX) NOT NULL,
-    level NVARCHAR(50),
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId),
-    FOREIGN KEY (LessonId) REFERENCES Lesson(LessonId)
+-- Tạo bảng Questions
+CREATE TABLE Questions (
+    QuestionID INT PRIMARY KEY IDENTITY(1,1),
+    SubjectID INT,
+    LessonID INT,
+    Content NVARCHAR(MAX) NOT NULL,
+    Level NVARCHAR(50),
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID),
+    FOREIGN KEY (LessonID) REFERENCES Lessons(LessonID)
 );
 GO
 
 -- Tạo bảng Test_Question
 CREATE TABLE Test_Question (
-    TestId INT,
-    QuestionId INT,
-    PRIMARY KEY (TestId, QuestionId),
-    FOREIGN KEY (TestId) REFERENCES Test(TestId),
-    FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
+    TestID INT,
+    QuestionID INT,
+    PRIMARY KEY (TestID, QuestionID),
+    FOREIGN KEY (TestID) REFERENCES Tests(TestID),
+    FOREIGN KEY (QuestionID) REFERENCES Questions(QuestionID)
 );
 GO
 
--- Tạo bảng Answer
-CREATE TABLE Answer (
-    AnswerId INT PRIMARY KEY IDENTITY(1,1),
-    QuestionId INT,
-    content NVARCHAR(MAX) NOT NULL,
+-- Tạo bảng Answers
+CREATE TABLE Answers (
+    AnswerID INT PRIMARY KEY IDENTITY(1,1),
+    QuestionID INT,
+    Content NVARCHAR(MAX) NOT NULL,
     isCorrect BIT,
-    FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
+    FOREIGN KEY (QuestionID) REFERENCES Questions(QuestionID)
 );
 GO
 
 -- Tạo bảng Result
 CREATE TABLE Result (
-    ResultId INT PRIMARY KEY IDENTITY(1,1),
-    QuestionId INT,
-    AttemptId INT,
+    ResultID INT PRIMARY KEY IDENTITY(1,1),
+    QuestionID INT,
+    AttemptID INT,
 	UserAnswer NVARCHAR(255),
-    FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId),
+    FOREIGN KEY (QuestionID) REFERENCES Questions(QuestionID),
 );
 GO
 
 -- Tạo bảng User_Attempt
 CREATE TABLE User_Attempt (
-    AttemptId INT PRIMARY KEY IDENTITY(1,1),
-    UserId INT,
-    TestId INT,
-    duration INT,
-    mark DECIMAL(5, 2),
-    FOREIGN KEY (UserId) REFERENCES Users(UserId),
-    FOREIGN KEY (TestId) REFERENCES Test(TestId)
+    AttemptID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT,
+    TestID INT,
+    Duration INT,
+    Mark DECIMAL(5, 2),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (TestID) REFERENCES Tests(TestID)
 );
 GO
 
 -- Tạo bảng Customer_Subject
 CREATE TABLE Customer_Subject (
-    UserId INT,
-    SubjectId INT,
-    progress DECIMAL(5, 2),
-    PRIMARY KEY (UserId, SubjectId),
-    FOREIGN KEY (UserId) REFERENCES Users(UserId),
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId)
+    UserID INT,
+    SubjectID INT,
+    Progress DECIMAL(5, 2),
+    PRIMARY KEY (UserID, SubjectID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID)
 );
 GO
 
 -- Tạo bảng Payment
 CREATE TABLE Payment (
-    PaymentId INT PRIMARY KEY IDENTITY(1,1),
-    UserId INT,
+    PaymentID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT,
     PaymentDate DATETIME DEFAULT GETDATE(),
     Amount DECIMAL(10, 2),
     PaymentMethod NVARCHAR(50),
-    FOREIGN KEY (UserId) REFERENCES Users(UserId)
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 GO
 
 -- Tạo bảng System_Setting
 CREATE TABLE System_Setting (
-    SettingId INT PRIMARY KEY IDENTITY(1,1),
-    UserId INT,
-    setting_key NVARCHAR(255) NOT NULL,
-    setting_value NVARCHAR(255),
-    created_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (UserId) REFERENCES Users(UserId)
+    SettingID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT,
+    Setting_Key NVARCHAR(255) NOT NULL,
+    Setting_Value NVARCHAR(255),
+    Created_At DATETIME DEFAULT GETDATE(),
+    Updated_At DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (UserId) REFERENCES Users(UserID)
 );
 GO
